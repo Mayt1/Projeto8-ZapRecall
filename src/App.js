f9827fe5cac09e129549ba53b838dcca11cf5e04
VictorUlian <yctor.ulian@gmail.com>
2022-03-18 18:34:06 -0300

    texte para ver se funciona colocar o css global no src

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import TelaInicial from './TelaInicial';
 import Headers from './Headers';
 import Footer from './Footer';
 
+import './styles/reset.css'
+import './styles/sytle.css'
+
+
+
 //Aqui q vou criar a logica para trocar de telas da tela inicial para os outros componentes
 
 function App() {
--- src/Flashcards.js
+++ src/Flashcards.js
@@ -1,32 +1,40 @@
 const pergunta = [{
+    question: 1,
     perguntafrente: "O que é JSX?",
     perguntatras:"Uma extensão de linguagem do JavaScript",
 },
-{
+{   
+    question: 2,
     perguntafrente: "O React é __ ",
     perguntatras:"uma biblioteca JavaScript para construção de interfaces",
 },
 {
+    question: 3,
     perguntafrente: "Componentes devem iniciar com  __ ",
     perguntatras:"letra maiúscula",
 },
 {
+    question: 4,
     perguntafrente: "Podemos colocar __ dentro do JSX",
     perguntatras:"expressões",
 },
 {
+    question: 5,
     perguntafrente: "O ReactDOM nos ajuda __",
     perguntatras: "interagindo com a DOM para colocar componentes React na mesma",
 },
 {
+    question: 6,
     perguntafrente: "Usamos o npm para __",
     perguntatras: "gerenciar os pacotes necessários e suas dependências",
 },
 {
+    question: 7,
     perguntafrente: "Usamos props para __",
     perguntatras: "passar diferentes informações para componentes ",
 },
 {
+    question: 8,
     perguntafrente: " Usamos estado (state) para __",
     perguntatras: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
 },
